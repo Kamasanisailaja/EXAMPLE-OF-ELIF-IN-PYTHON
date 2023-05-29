473f40cef301411b435748fef6ea4d0a9b4dd83a
Kamasanisailaja <106640335+Kamasanisailaja@users.noreply.github.com>
2023-05-29 18:55:50 +0530

    Add files via upload

---
+++ 3a.py
@@ -0,0 +1,10 @@
+m=int(input("enter the amount"))
+if m<=2000:
+  print("no discount")
+elif m>2000 and m<=5000:
+    print("5% discount will be available")
+elif m>5000 and m<=10000:
+    print("10% discount will be available")
+elif m>10000:
+    print("15% discount will be available")
+    
